56030581cb72f4b6fc771e65f28e6f2e59dda788
Mark Holmquist <marktraceur@gmail.com>
2010-10-05 02:49:33 -0700

    Changed the whole structure to allow use of stdin and stdout, and pushed target and source files to options. Also clarified some things in the README.

--- cols.cpp
+++ cols.cpp
@@ -13,46 +13,20 @@ Original code written by Mark Holmquist at the University of Redlands in 2010.
 using namespace std;
 
 void usage() {
-  cout << "Usage: cols [-c column_number] sourcefile targetfile" << endl;
+  cout << "Usage: cols [-c column_number] [-s sourcefile] [-t targetfile]\n";
 }
 
-int main(int argc, char const **argv) {
-  string origfile = "";
-  string targfile = "";
-  int cols = 80;
+void help() {
+  cout << "cols [infile [outfile]]-- take STDIN and cut it down to a specified number of columns, preserving whole words, and output the result to STDOUT.\n\nOPTIONS\n  -h/-? : Display this help message.\n  -c <cols> : Specify the number of columns to preserve (default 80).\n  -s <sourcefile> : Designate a source file, rather than taking input from STDIN.\n  -t <targetfile> : Desginate a target file, rather than outputting to STDOUT.\n";
+}
 
-  for (int i = 1; i < argc; i++) { // Parsing all arguments.
-    if (argv[i][0] == '-') { // Options, denoted by a hyphen. All single-letter.
-      if (argv[i][1] == 'c')
-	cols = atoi(argv[++i]);
-      else { // Send them to boarding school if they use any unknown options.
-	usage();
-	return 0;
-      }
-    }
-    else {
-      if (origfile == "")
-	origfile = argv[i];
-      else
-	targfile = argv[i];
-    }
-  }
-
-  if (origfile == "") { // Check if we have an input file, else send them to boot camp.
-    usage();
-    return 0;
-  }
-
-  if (targfile == "") // Acceptable to only use one file.
-    targfile = origfile;
-
-  ifstream fin(origfile.c_str());
+void parse(istream &istrm, ostream &ostrm, int cols) {
   string tmp, tmp1, endout = "";
   bool islong = false; // Needed to preserve the endlines in the original file
   int loc=0;
 
-  while (!fin.eof()) {
-    getline(fin, tmp);
+  while (!istrm.eof()) {
+    getline(istrm, tmp);
     while (tmp.size() > cols) {
       islong = true;
       tmp1 = tmp.substr(0, cols);
@@ -69,18 +43,60 @@ int main(int argc, char const **argv) {
       }
     }
     endout.append(tmp); // Now that we know what to append, throw it in.
-    if ((!fin.eof() || (tmp == "\n" && fin.eof()))) { // Make sure we aren't adding endlines we don't need
+    if ((!istrm.eof() || (tmp == "\n" && istrm.eof()))) { // Make sure we aren't adding endlines we don't need
       endout.append("\n");
       islong = false;
     }
-   }
+  }
+  ostrm << endout;
+}
+  
 
-  fin.close();
+int main(int argc, char const **argv) {
+  string origfile = "";
+  string targfile = "";
+  int cols = 80;
 
-  ofstream fout(targfile.c_str());
-  fout << endout;
-
-  fout.close();
+  for (int i = 1; i < argc; i++) { // Parsing all arguments.
+    if (argv[i][0] == '-') { // Options, denoted by a hyphen. All single-letter.
+      if (argv[i][1] == 'c')
+	cols = atoi(argv[++i]);
+      else if (argv[i][1] == 's') {
+	origfile = argv[++i];
+      }
+      else if (argv[i][1] == 't') {
+	targfile = argv[++i];
+      }
+      else if (argv[i][1] == 'h' || argv[i][1] == '?') {
+	help();
+	return 0;
+      }
+      else { // Send them to boarding school if they use any unknown options.
+	usage();
+	return 0;
+      }
+    }
+  }
+  if (origfile != "") {
+    ifstream fin(origfile.c_str());
+    if (targfile != "") {
+      ofstream fout(targfile.c_str());
+      parse(fin, fout, cols);
+      fout.close();
+    }
+    else
+      parse(fin, cout, cols);
+    fin.close();
+  }
+  else {
+    if (targfile != "") {
+      ofstream fout(targfile.c_str());
+      parse(cin, fout, cols);
+      fout.close();
+    }
+    else
+      parse(cin, cout, cols);
+  }
 
   return 0;
 }
